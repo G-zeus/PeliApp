10e052d4b2356e3bf186c8611ac66a44fb59fe38
Saul Zamora <programadorsaul@gmail.com>
2023-09-18 13:48:23 -0600

    feat: movi controller

---
+++ back/src/controllers/movie.py
@@ -0,0 +1,4 @@
+class Movie:
+
+    def get_popular(self, page: int = 0):
+        return {"data": {"page": page, "movies": ""}}
--- back/src/routes/movies.py
+++ back/src/routes/movies.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 from typing import Union
+from ..controllers.movie import Movie
 
 movies = APIRouter(prefix="/api/movies")
+controller = Movie()
 
 
 @movies.get("/popular")
 def get_popular_movies(page: Union[int, None] = None):
-    return {"data": {"page": page, "movies": ""}}
+    return controller.get_popular(page=page)
 
 
 @movies.get("/{id}")
