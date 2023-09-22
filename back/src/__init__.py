c48a8e62daf7372d6fd0399ad6f5f4aaeb185cea
Saul Zamora <programadorsaul@gmail.com>
2023-09-22 09:34:58 -0600

    feat: add cors config

--- back/src/__init__.py
+++ back/src/__init__.py
@@ -1,11 +1,13 @@
 from .routes.movies import movies
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .config.config import get_settings
 
 app = FastAPI()
 
 
 def create_app():
+    set_cors()
     get_routes()
 
     @app.get("/")
@@ -17,3 +19,13 @@ def create_app():
 
 def get_routes():
     app.include_router(movies)
+
+
+def set_cors():
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
